661ba0662484a89b28be52a9be56b2be779be9d0
jjy <673624736@qq.com>
2019-12-14 01:40:07 +0800

    register class to admin

--- music_management_system/music/admin.py
+++ music_management_system/music/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Singers
+from .models import Songs
+from .models import Albums
+from .models import Composers
+from .models import Lyricwritters
 
 # Register your models here.
+admin.site.register(Songs)
+admin.site.register(Singers)
+admin.site.register(Albums)
+admin.site.register(Composers)
+admin.site.register(Lyricwritters)
+
+
+
