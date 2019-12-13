01f8abcbe49bbdfab81e60e78ec2bd19a420417e
jjy <673624736@qq.com>
2019-12-13 23:19:13 +0800

    add html

--- music_management_system/music/urls.py
+++ music_management_system/music/urls.py
@@ -4,11 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    # path('p/<int:player_api_id>/', views.player_detail, name='player_detail'),
-    # path('m/<int:match_id>/',views.match_detail,name='match_detail'),
-    # path('t/<int:team_api_id>/',views.team_detail,name='team_detail'),
-    # path('l/<int:league_id>/',views.league_detail,name='league_detail'),
-    # path('srp',views.player_search,name='player_search'),
-    # path('srt',views.team_search,name='team_search')
+    path('singer/',views.singer,name='singer'),
+    path('composer/',views.composer,name='composer'),
+    path('lyricist/',views.lyricist,name='lyricist'),
+    path('album/',views.album,name='album')
 ]
--- music_management_system/music/views.py
+++ music_management_system/music/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from . import models
-# Create your views here.
-# def index(request):
-#     try:
-#         res = {}
-#         res['top_players'] = Player.objects.all()[:5]
-#         res['top_teams'] = Team.objects.all()[:5]
-#         res['recent_matchs'] = Match.objects.all().order_by('-date')[:5]
-#         res['leagues']=League.objects.all()
-#         template = loader.get_template('player/index.html')
-#     except:
-#         raise Http404("Oops! Resource doesn't exist. If you believe this is a bug or error, contact\
-#          maijj6@mail2.sysu.edu.cn please.")
-#     return HttpResponse(template.render(res, request))
 
 def index(request):
     singers_list = models.Singers.objects.order_by('-singer_name')[:5]
     # context = {'latest_question_list': latest_question_list}
     context={'singers_list':singers_list}
     return render(request, 'music/index.html', context)
+
+def singer(request):
+    singers_list = models.Singers.objects.order_by('-singer_name')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    context={'singers_list':singers_list}
+    return render(request, 'music/singer.html', context)
+
+def composer(request):
+    singers_list = models.Singers.objects.order_by('-singer_name')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    context={'singers_list':singers_list}
+    return render(request, 'music/composer.html', context)
+
+def lyricist(request):
+    singers_list = models.Singers.objects.order_by('-singer_name')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    context={'singers_list':singers_list}
+    return render(request, 'music/lyricist.html', context)
+
+def album(request):
+    singers_list = models.Singers.objects.order_by('-singer_name')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    context={'singers_list':singers_list}
+    return render(request, 'music/album.html', context)
